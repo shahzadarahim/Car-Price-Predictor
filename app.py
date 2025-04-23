591c4d58ea02de01b2bd4b273ae290957e9b99b6
Shahzada Rahim <3rshazi@gmail.com>
2025-04-23 15:11:09 +0530

    small mistakes rectified

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ def predict():
         input_df = pd.DataFrame({
             'Brand' : [Brand],
             'Model' : [Model],
-            'Fuel' : [Fuel],
+            'Fuel_Type' : [Fuel],
             'Transmission' : [Transmission],
             'Year' : [Year],
             'EngineSize' : [EngineSize],
@@ -42,20 +42,22 @@ def predict():
         })
 
         # Apply mean encoding for Brand and Model
-        input_df['Brand_encoded'] = input_df['Brand'].map(Brand_Encoder)
-        input_df['Model_encoded'] = input_df['Model'].map(Model_Encoder)
-        input_df['Brand_encoded'] = input_df['Brand_encoded'].fillna(input_df['Brand_encoded'].mean())
-        input_df['Model_encoded'] = input_df['Model_encoded'].fillna(input_df['Model_encoded'].mean())
-        input_df.drop(['Brand', 'Model'], axis=1, inplace=True)
+        input_df['Brand'] = input_df['Brand'].map(Brand_Encoder)
+        input_df['Model'] = input_df['Model'].map(Model_Encoder)
+        input_df['Brand'] = input_df['Brand'].fillna(input_df['Brand'].mean())
+        input_df['Model'] = input_df['Model'].fillna(input_df['Model'].mean())
+        # input_df.drop(['Brand', 'Model'], axis=1, inplace=True)
 
         # One Hot encode Fuel and Transmission
-        input_df.rename(columns={'Fuel': 'Fuel_Type'}, inplace=True)
+        # input_df.rename(columns={'Fuel': 'Fuel_Type'}, inplace=True)
         cat_columns = ['Transmission', 'Fuel_Type']
         encoded_array = OneHot_Encoder.transform(input_df[cat_columns])
-        encoded_df = pd.DataFrame(encoded_array, columns=OneHot_Encoder.get_feature_names_out(cat_columns))
+        encoded_df = pd.DataFrame(encoded_array, 
+                                  columns = OneHot_Encoder.get_feature_names_out(cat_columns),
+                                  index = input_df.index)
         
         # Merge encoded columns with input data
-        input_df_encoded = input_df.drop(columns=cat_columns).reset_index(drop=True)
+        input_df_encoded = input_df.drop(columns=cat_columns)
         input_data = pd.concat([input_df_encoded, encoded_df], axis=1)
 
         # Make prediction
